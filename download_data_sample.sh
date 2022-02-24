392aeb94bf7dcbacee47295b2a11b3823847a1e9
iain-mackie <i.mackie@me.com>
2022-02-24 12:25:47 +0000

    Changing download paths

--- download_data_sample.sh
+++ download_data_sample.sh
@@ -7,4 +7,4 @@ echo "Downloading KILT (Entity KB) -> SAMPLE..."
 wget https://codec-public-data.s3.amazonaws.com/sample_kilt_knowledgesource.json -P "./data"
 
 echo "Downloading Entity Links (Docs -> Ents) -> SAMPLE..."
-wget https://codec-public-data.s3.amazonaws.com/sample_codec_entity_links.jsonl -P "./data"
+wget https://codec-public-data.s3.amazonaws.com/sample_comets_entity_link.jsonl -P "./data"
