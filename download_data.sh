43f6e128c2cbaa29a4166d76fc124ae4fcd23aa5
Iain Mackie <57227106+iain-mackie@users.noreply.github.com>
2024-12-01 13:27:51 +0000

    Update download_data.sh
    Updating download scipt

--- download_data.sh
+++ download_data.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "Downloading CODEC (Documents Corpus)..."
-wget  https://codec-public-data.s3.amazonaws.com/codec_documents.jsonl -P "./data"
+echo "Downloading CODEC (Documents Corpus) from IR-datasets or Huggingface"
 
 echo "Downloading KILT (Entity KB)..."
 wget  wget http://dl.fbaipublicfiles.com/KILT/kilt_knowledgesource.json -P "./data"
-
-echo "Downloading Entity Links (Docs -> Ents)..."
-wget  https://codec-public-data.s3.amazonaws.com/codec_entity_links.jsonl -P "./data"
 
